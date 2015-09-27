32427ee0fd7621a1e5d73ffece17f2d2c7cb977a
Sharipov Ruslan <sharipov.ru@gmail.com>
2015-09-27 13:47:03 +0300

    correct names

--- player.rb
+++ player.rb
@@ -2,7 +2,7 @@ class Player
   VERSION = 'Lazy bot'
 
   def bet_request(game_state)
-    brain = PokerBrain.new
+    brain = PokerBrain.new(game_state)
     brain.make_decision(game_state)
   rescue => e
     puts e
@@ -16,11 +16,16 @@ end
 
 class PokerBrain
   STRATEGIES = {
-    2 => AgressivePlayStrategy,
-    3 => AgressivePlayStrategy,
-    4 => FoldStrategy,
-    5 => FoldStrategy,
-    6 => FoldStrategy,
+    2 => InemOldStrategy,
+    3 => InemOldStrategy,
+    4 => InemOldStrategy,
+    5 => InemOldStrategy,
+    6 => InemOldStrategy,
+    # 2 => AgressivePlayStrategy,
+    # 3 => AgressivePlayStrategy,
+    # 4 => FoldStrategy,
+    # 5 => FoldStrategy,
+    # 6 => FoldStrategy,
   }
 
   attr_reader :game_state
@@ -31,13 +36,37 @@ class PokerBrain
 
   def make_decision(game_state)
     game = Game.new(game_state)
-    strategy = STRATEGIES[game.active_players_count.to_sym]
+    strategy = STRATEGIES[game.active_players_count]
     strategy.execute(game_state)
+  end
+end
+
+class Game
+  attr_reader :game_state
+
+  def initialzie(game_state)
+    @game_state = game_state
+  end
+
+  def active_players_count
+    6
   end
 end
 
 class InemOldStrategy
   def self.execute(game_state)
     rand(100)+200
+  end
+end
+
+class AgressivePlayStrategy
+  def self.execute(game_state)
+    1000000
+  end
+end
+
+class FoldStrategy
+  def self.execute(game_state)
+    0
   end
 end
